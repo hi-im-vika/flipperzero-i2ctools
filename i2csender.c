45bfa999a51419c72d8cc353af05ff43c0c20aab
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-16 21:58:35 -0700

    specify default number of bytes to read

--- i2csender.c
+++ i2csender.c
@@ -3,13 +3,15 @@
 void i2c_send(i2cSender* i2c_sender) {
     furi_hal_i2c_acquire(I2C_BUS);
     uint8_t address = i2c_sender->scanner->addresses[i2c_sender->address_idx] << 1;
+    // dynamically allocate rx buffer according to setting
+    uint8_t* rx_buf = malloc(i2c_sender->bytes_to_rx * sizeof(uint8_t));
     i2c_sender->error = furi_hal_i2c_trx(
         I2C_BUS,
         address,
         &i2c_sender->value,
         sizeof(i2c_sender->value),
-        i2c_sender->recv,
-        sizeof(i2c_sender->recv),
+        &rx_buf[0],
+        sizeof(rx_buf),
         I2C_TIMEOUT);
     furi_hal_i2c_release(I2C_BUS);
     i2c_sender->must_send = false;
@@ -18,6 +20,8 @@ void i2c_send(i2cSender* i2c_sender) {
 
 i2cSender* i2c_sender_alloc() {
     i2cSender* i2c_sender = malloc(sizeof(i2cSender));
+    // set default num. bytes to read to existing behaviour
+    i2c_sender->bytes_to_rx = 2;
     i2c_sender->must_send = false;
     i2c_sender->sended = false;
     return i2c_sender;
